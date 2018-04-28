4601d66acb4a3f636489ba829bf1d621e3ffecf9
TeeSeal <teesealz@gmail.com>
2018-04-28 16:55:40 +0300

    implement sessions

--- lib/origami_web/controllers/user_controller.ex
+++ lib/origami_web/controllers/user_controller.ex
@@ -2,54 +2,21 @@ defmodule OrigamiWeb.UserController do
   use OrigamiWeb, :controller
 
   alias Origami.Auth
-  alias Origami.Auth.User
 
   action_fallback OrigamiWeb.FallbackController
-
-  def index(conn, _params) do
-    users = Auth.list_users()
-    render(conn, "index.json", users: users)
-  end
-
-  def create(conn, %{"user" => user_params}) do
-    with {:ok, %User{} = user} <- Auth.create_user(user_params) do
-      conn
-      |> put_status(:created)
-      |> put_resp_header("location", user_path(conn, :show, user))
-      |> render("show.json", user: user)
-    end
-  end
-
-  def show(conn, %{"id" => id}) do
-    user = Auth.get_user!(id)
-    render(conn, "show.json", user: user)
-  end
-
-  def update(conn, %{"id" => id, "user" => user_params}) do
-    user = Auth.get_user!(id)
-
-    with {:ok, %User{} = user} <- Auth.update_user(user, user_params) do
-      render(conn, "show.json", user: user)
-    end
-  end
-
-  def delete(conn, %{"id" => id}) do
-    user = Auth.get_user!(id)
-    with {:ok, %User{}} <- Auth.delete_user(user) do
-      send_resp(conn, :no_content, "")
-    end
-  end
 
   def login(conn, %{"access_token" => access_token}) do
     case Auth.authenticate_user(access_token) do
       {:ok, user} ->
         conn
+        |> put_session(:current_user_id, user.id)
         |> put_status(:ok)
         |> put_resp_header("location", user_path(conn, :show, user))
         |> render("show.json", user: user)
 
       {:error, message} ->
         conn
+        |> delete_session(:current_user_id)
         |> put_status(:unauthorized)
         |> render(OrigamiWeb.ErrorView, "401.json", message: message)
     end
--- lib/origami_web/router.ex
+++ lib/origami_web/router.ex
@@ -3,12 +3,35 @@ defmodule OrigamiWeb.Router do
 
   pipeline :api do
     plug :accepts, ["json"]
+    plug :fetch_session
+  end
+
+  pipeline :api_auth do
+    plug :ensure_authenticated
   end
 
   scope "/", OrigamiWeb do
     pipe_through :api
 
     get "/login", UserController, :login
-    resources "/users", UserController, except: [:new, :edit]
+  end
+
+  scope "/", OrigamiWeb do
+    pipe_through [:api, :api_auth]
+
+    resources "/tags", TagController
+  end
+
+  defp ensure_authenticated(conn, _opts) do
+    current_user_id = get_session(conn, :current_user_id)
+
+    if current_user_id do
+      conn
+    else
+      conn
+      |> put_status(:unauthorized)
+      |> render(OrigamiWeb.ErrorView, "401.json", message: "Unauthenticated user.")
+      |> halt()
+    end
   end
 end
